1f9952b7ab591034bd6d232c9fc569fba2513647
RyanDBarnett <conner2468@gmail.com>
2022-06-07 16:32:43 -0600

    On page load random cover image, title, and descriptors appear

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
 
 // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  createCover()
 ];
-var currentCover;
-
+var currentCover = savedCovers[0];
+updateCover(currentCover);
 // Add your event listeners here 👇
 
 
 // Create your event handlers and other functions here 👇
+function createCover() {
+  var coverImgSrc = covers[getRandomIndex(covers)];
+  var title = titles[getRandomIndex(titles)];
+  var descriptor1 = descriptors[getRandomIndex(descriptors)];
+  var descriptor2 = descriptors[getRandomIndex(descriptors)];
+  return new Cover(coverImgSrc, title, descriptor1, descriptor2);
+}
 
+function updateCover(cover) {
+  coverImage.src = cover.cover;
+  coverTitle.innerText = cover.title;
+  tagline1.innerText = cover.tagline1;
+  tagline2.innerText = cover.tagline2;
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
